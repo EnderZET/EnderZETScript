98638044f40b65b93a5ae87b0f15d517964b508d
EnderZET <72235078+EnderZET@users.noreply.github.com>
2020-10-18 10:16:52 +0700

    Add files via upload

---
+++ 1.2/info-vs2.sh
@@ -0,0 +1,19 @@
+#!/system/bin/sh
+
+clear
+toilet -f big -F gay MCScript
+echo
+echo "Created By McFrozie"
+echo "CopyRight Reserved 2020@2.1.1"
+echo
+echo "Thanks To EnderZET"
+echo "Type 00 To Exit"
+echo "Your Phone Info"
+echo
+neofetch
+echo
+read -p "> " pg
+if [ $pg = 00 ]
+then
+sh vs2.sh
+fi
